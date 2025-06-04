69ac55852a5fb9b300081816b797f7142e2f03a1
Kristian3123 <po-km@abv.bg>
2025-06-05 02:06:35 +0300

    modified:   src/app/app.component.scss 	modified:   src/app/models/project-data.ts

--- src/app/models/project-data.ts
+++ src/app/models/project-data.ts
@@ -6,7 +6,7 @@ export const PROJECTS: Project[] =[
         id:0,
       title: 'Portfolio Website',
       summary: 'An Angular project showcasing projects, skills, and results — highlighting expertise and impact',
-      description: 'A curated collection of my most impactful projects, showcasing skills, expertise, and achievements across [your field, e.g., design, development, marketing]. Demonstrates problem-solving, creativity, and measurable results. ',
+      description: 'Acurated collection of my most impactful projects, showcasing skills, expertise, and achievements across my field of programming. Demonstrates problem-solving, creativity, and measurable results. ',
       
       tags:[Tag.Angular, Tag.TypeScript],
       images: ['assets/projects/Portfolio/Portfolio_Home.PNG','assets/projects/Portfolio/Portfolio_Projects.PNG','assets/projects/Portfolio/Portfolio_Resume.PNG'],
